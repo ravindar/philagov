c238c060aad5ee4902f4096f588663f2790e57e4
Ravindar Gujral <ravindargujral@gmail.com>
2017-08-28 18:16:11 -0400

    concurrent api calls

---
+++ main.go
@@ -0,0 +1,99 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"golang.org/x/net/html"
+	"net/http"
+	"bufio"
+	"os"
+	"log"
+	"strings"
+)
+
+var (
+	address []string
+	unitno  []string
+)
+
+type ResponseMsg struct {
+	Features []Feature `json:"features"`
+}
+
+type Feature struct {
+	Properties Properties `json:"properties"`
+}
+
+type Properties struct {
+	Address   string `json:"street_address"`
+	UnitNumber string `json:"unit_num"`
+	OPANumber string `json:"opa_account_num"`
+}
+
+// readLines reads a whole file into memory
+// and returns a slice of its lines.
+func readLines(path string) ([]string, error) {
+  file, err := os.Open(path)
+  if err != nil {
+    return nil, err
+  }
+  defer file.Close()
+
+  var lines []string
+  scanner := bufio.NewScanner(file)
+  for scanner.Scan() {
+    lines = append(lines, scanner.Text())
+  }
+  return lines, scanner.Err()
+}
+
+func crawl(url string, ch chan ResponseMsg) {
+	resp, err := http.Get(url)
+
+	if err != nil {
+		fmt.Println("ERROR: Failed to crawl \"" + url + "\"")
+		close(ch)
+		return
+	}
+
+	defer resp.Body.Close()
+
+	var response ResponseMsg
+
+	err = json.NewDecoder(resp.Body).Decode(&response)
+
+	if err != nil {
+		fmt.Println("Invalid JSON response", err)
+		close(ch)
+	}
+
+	ch <- response
+	return
+}
+
+func main() {
+	lines, err := readLines("address.in.txt")
+  	if err != nil {
+    		log.Fatalf("readLines: %s", err)
+  	}
+
+	// Channels
+	chUrls := make(chan ResponseMsg)
+
+	for i, line := range lines {
+		seedUrls := "https://api.phila.gov/ais_ps/v1/addresses/"
+		fmt.Println("line - ", i)
+
+		addresses := strings.Split(line, ",")
+
+		seedUrls = seedUrls + html.EscapeString(strings.TrimSpace(addresses[0])) + "?include_units=" + strings.TrimSpace(addresses[1]) + "&opa_only="
+		fmt.Println("Feed URL - ", seedUrls)
+
+		go crawl(seedUrls, chUrls)
+
+		response := <-chUrls
+		fmt.Println(response.Features)
+  	}
+
+	defer close(chUrls)
+}
